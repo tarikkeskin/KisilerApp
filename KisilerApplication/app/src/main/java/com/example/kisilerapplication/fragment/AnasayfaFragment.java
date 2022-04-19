4d230291b16b3a2d29ebeeb429c0a62bcc109b55
tarikkeskin <tarik.keskin007@gmail.com>
2022-04-19 18:02:05 +0300

    navigation-navigate_view

--- KisilerApplication/app/src/main/java/com/example/kisilerapplication/fragment/AnasayfaFragment.java
+++ KisilerApplication/app/src/main/java/com/example/kisilerapplication/fragment/AnasayfaFragment.java
@@ -3,21 +3,34 @@ package com.example.kisilerapplication.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kisilerapplication.R;
+import com.example.kisilerapplication.databinding.FragmentAnasayfaBinding;
 
 
 public class AnasayfaFragment extends Fragment {
 
+    private FragmentAnasayfaBinding tasarim;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anasayfa, container, false);
+
+        tasarim = FragmentAnasayfaBinding.inflate(inflater, container, false);
+
+        tasarim.toolbarAnasayfa.setTitle("Kişiler");
+
+        tasarim.fab.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.kisiKayitGecis);
+
+        });
+
+        return tasarim.getRoot();
     }
 }
